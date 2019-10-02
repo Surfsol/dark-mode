a9d559c62ed9ceae8fedbdef882677a71ac6be58
Surfsol <surfsol@protonamil.com>
2019-10-02 16:47:54 -0300

    Add useLocalStorage.js and useDarkMode.js

---
+++ src/components/hooks/useDarkMode.js
@@ -0,0 +1,11 @@
+import {useLocalStorage} from './useLocalStorage'
+//use effect hook to manipulate the DOM to DarkMode
+import React, {useEffect} from 'react'
+
+
+
+const useDarkMode () =>{
+    const[someValue, setSomeValue]= useLocalStorage('enter your key')
+
+
+}
